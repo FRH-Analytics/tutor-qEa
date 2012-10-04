b613e4c70262d1f9ed0c8abae95316fcea791c9b
Augusto QM <augustoqmacedo@gmail.com>
2012-10-04 16:29:55 -0300

    Projeto no Eclipse.

---
+++ TutorQeA/src/TagSelection.java
@@ -0,0 +1,22 @@
+import processing.core.PApplet;
+
+
+public class TagSelection extends PApplet{
+
+	private static final long serialVersionUID = 1L;
+
+	public void setup() {
+		size(1380, 900);
+
+		cursor(ARROW);
+		rectMode(CORNERS);
+		smooth();
+		noStroke();
+		frameRate(30);
+
+	}
+
+	public void draw() {
+		background(100, 100);
+	}
+}
