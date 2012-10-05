1bddaa7eb4424ea1385937d7d47d080a3d07fa86
Augusto QM <augustoqmacedo@gmail.com>
2012-10-05 14:05:50 -0300

    Lib de leitura de arquivos CSV.

--- TutorQeA/src/TagSelection.java
+++ TutorQeA/src/TagSelection.java
@@ -1,24 +1,68 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.gicentre.utils.colour.ColourTable;
+import org.gicentre.utils.stat.BarChart;
+
 import processing.core.PApplet;
+import processing.core.PFont;
+import au.com.bytecode.opencsv.CSVReader;
 
-
-public class TagSelection extends PApplet{
+public class TagSelection extends PApplet {
 
 	private static final long serialVersionUID = 1L;
 
+	BarChart barChart;
+	float[] values = { 76, 24, 39, 18, 20 };
+	ColourTable barColours = ColourTable.getPresetColourTable(ColourTable.PAIRED_12);
+
+	csvFilePath = "";
+	
+	/**
+	 * Initialises the sketch and loads data into the chart.
+	 */
 	public void setup() {
-		size(1380, 900);
-
-		cursor(ARROW);
-		rectMode(CORNERS);
+		size(600, 500);
 		smooth();
-		noStroke();
-		frameRate(30);
 
+		barChart = new BarChart(this);
+		barChart.setData(values);
+
+		// Scaling
+		barChart.setMinValue(0);
+		barChart.setMaxValue(100);
+
+		// Axis appearance
+		PFont font = createFont("Serif", 10);
+		textFont(font, 10);
+
+		barChart.showValueAxis(true);
+//		barChart.setValueFormat("#%");
+		barChart.showCategoryAxis(true);
 		
-		
+		// Bar colours and appearance
+		barChart.setBarColour(values, barColours);
+		barChart.setBarGap(4);
+
+		// Bar layout
+		barChart.transposeAxes(false);
 	}
 
+	void readCommunitiesFile() throws IOException {
+
+		CSVReader reader = new CSVReader(new FileReader(csvFilePath));
+
+		String[] nextLine = reader.readNext();
+		while ((nextLine = reader.readNext()) != null) {
+		}
+	}
+	
+	/**
+	 * Draws the chart in the sketch
+	 */
 	public void draw() {
-		background(100, 100);
+
+		background(255);
+		barChart.draw(15, 15, width - 30, height - 30);
 	}
 }
