30c71e3371523405aebddbec8a9558e8e939b7ca
Augusto QM <augustoqmacedo@gmail.com>
2012-10-09 14:13:40 -0300

    Sketch3 with Accordion = Doing Some ControlP5 examples

--- TutorQeA/src/Sketch3.java
+++ TutorQeA/src/Sketch3.java
@@ -5,15 +5,17 @@ import java.util.ArrayList;
 import processing.core.PApplet;
 import processing.core.PFont;
 import util.QeAData;
+import controlP5.Accordion;
 import controlP5.ControlP5;
+import controlP5.Group;
 
 public class Sketch3 extends PApplet {
 
 	private static final long serialVersionUID = 1L;
 	int defaultFontSize;
 
-	int fullBoxXOrigin;
-	int fullBoxYOrigin;
+	// int fullBoxXOrigin;
+	// int fullBoxYOrigin;
 
 	int questionRectHeight;
 	int questionRectPadding;
@@ -21,7 +23,8 @@ public class Sketch3 extends PApplet {
 	ArrayList<Integer> questionIds;
 
 	ControlP5 cp5;
-	
+	Accordion accordion;
+
 	public void setup() {
 		size(800, 500);
 		smooth();
@@ -32,12 +35,14 @@ public class Sketch3 extends PApplet {
 		textFont(font, defaultFontSize);
 
 		// Full box
-		fullBoxXOrigin = 5;
-		fullBoxYOrigin = 5;
+		// fullBoxXOrigin = 5;
+		// fullBoxYOrigin = 5;
+
+		cp5 = new ControlP5(this);
 
 		// Question Rectangles
 		questionRectHeight = 75;
-		questionRectPadding = fullBoxXOrigin + 5;
+		questionRectPadding = 5;
 
 		// Instantiates the objects
 		questionIds = new ArrayList<Integer>();
@@ -55,49 +60,83 @@ public class Sketch3 extends PApplet {
 		}
 
 		// Testing...
-		questionIds.add(1);
-		questionIds.add(2);
-		questionIds.add(4);
+		ArrayList<Integer> newIds = new ArrayList<Integer>();
+		newIds.add(1);
+		newIds.add(2);
+		newIds.add(4);
+
+		updateQuestions(newIds);
 	}
 
 	public void draw() {
 		background(255);
+	}
 
-		fill(255);
-		rect(fullBoxXOrigin, fullBoxYOrigin, width - fullBoxXOrigin, height
-				- fullBoxYOrigin, 5, 5, 5, 5);
+	private void drawQuestionsAccordion() {
+		int qId;
+		if (questionIds.size() > 0) {
 
-		for (int j = 0; j < questionIds.size(); j++) {
-			drawQuestionRectangles(j);
+			accordion = cp5.addAccordion("QuestionsAccordion")
+					.setPosition(20, 20).setWidth(width - 50);
+
+			Group g1;
+			for (int j = 0; j < questionIds.size(); j++) {
+				qId = questionIds.get(j);
+
+				g1 = cp5.addGroup(String.valueOf(qId))
+						.setLabel(
+								QeAData.getQuestionIdsToData().get(qId)
+										.getTitle())
+						.setBackgroundColor(color(225))
+						.setBackgroundHeight(100).setBarHeight(30);
+
+				PFont pfont = createFont("Arial", 20, true); // use true/false
+																// for
+				// smooth/no-smooth
+				controlP5.ControlFont font = new controlP5.ControlFont(pfont,
+						20);
+
+				g1.getCaptionLabel().toUpperCase(false).setLetterSpacing(3).setFont(font);
+
+				accordion.addItem(g1);
+			}
+
+			accordion.open();
+
+			// use Accordion.MULTI to allow multiple group
+			// to be open at a time.
+			accordion.setCollapseMode(Accordion.MULTI);
 		}
-
-//		noLoop();
 	}
 
-	private void drawQuestionRectangles(int qIndex) {
-
-		int xRectPadding = questionRectPadding;
-		int yRectPadding = questionRectPadding
-				+ ((questionRectHeight + questionRectPadding) * qIndex);
-		int rectWidth = width - (2 * questionRectPadding) - 500;
-
-		int xTitlePadding = 2 * questionRectPadding;
-		int yTitlePadding = yRectPadding + questionRectPadding
-				+ questionRectPadding;
-
-		int qId = questionIds.get(qIndex);
-
-		fill(255);
-		rect(xRectPadding, yRectPadding, rectWidth, questionRectHeight);
-
-		fill(0);
-		text(QeAData.getQuestionIdsToData().get(qId).getTitle(), xTitlePadding,
-				yTitlePadding, rectWidth - questionRectPadding, yRectPadding
-						+ questionRectHeight - questionRectPadding);
-	}
+	// private void drawQuestionRectangles(int qIndex) {
+	//
+	// int xRectPadding = questionRectPadding;
+	// int yRectPadding = questionRectPadding
+	// + ((questionRectHeight + questionRectPadding) * qIndex);
+	// int rectWidth = width - (2 * questionRectPadding) - 500;
+	//
+	// int xTitlePadding = 2 * questionRectPadding;
+	// int yTitlePadding = yRectPadding + questionRectPadding
+	// + questionRectPadding;
+	//
+	// int qId = questionIds.get(qIndex);
+	//
+	// fill(255);
+	// rect(xRectPadding, yRectPadding, rectWidth, questionRectHeight);
+	//
+	// fill(0);
+	// text(QeAData.getQuestionIdsToData().get(qId).getTitle(), xTitlePadding,
+	// yTitlePadding, rectWidth - questionRectPadding, yRectPadding
+	// + questionRectHeight - questionRectPadding);
+	// }
 
 	@SuppressWarnings("unchecked")
-	void updateQuestions(ArrayList<Integer> newQuestionIds) {
-		questionIds = (ArrayList<Integer>) newQuestionIds.clone();
+	public void updateQuestions(ArrayList<Integer> newQuestionIds) {
+		if (!newQuestionIds.equals(questionIds)) {
+			cp5.remove("QuestionsAccordion");
+			questionIds = (ArrayList<Integer>) newQuestionIds.clone();
+			drawQuestionsAccordion();
+		}
 	}
 }
--- TutorQeA/src/TheAccordion.java
+++ TutorQeA/src/TheAccordion.java
@@ -1,6 +1,5 @@
 import processing.core.PApplet;
 import controlP5.Accordion;
-import controlP5.ControlKey;
 import controlP5.ControlP5;
 import controlP5.Group;
 
@@ -59,20 +58,6 @@ public class TheAccordion extends PApplet{
 	                .setBackgroundColor(color(0, 64))
 	                .setBackgroundHeight(150)
 	                ;
-	  
-	  cp5.addRadioButton("radio")
-	     .setPosition(10,20)
-	     .setItemWidth(20)
-	     .setItemHeight(20)
-	     .addItem("black", 0)
-	     .addItem("red", 1)
-	     .addItem("green", 2)
-	     .addItem("blue", 3)
-	     .addItem("grey", 4)
-	     .setColorLabel(color(255))
-	     .activate(2)
-	     .moveTo(g2)
-	     ;
 
 	  // group number 3, contains a bang and a slider
 	  Group g3 = cp5.addGroup("myGroup3")
@@ -112,14 +97,6 @@ public class TheAccordion extends PApplet{
 	                 .addItem(g3)
 	                 ;
 	                 
-//	  cp5.mapKeyFor(new ControlKey() {public void keyEvent() {accordion.open(0,1,2);}}, 'o');
-//	  cp5.mapKeyFor(new ControlKey() {public void keyEvent() {accordion.close(0,1,2);}}, 'c');
-//	  cp5.mapKeyFor(new ControlKey() {public void keyEvent() {accordion.setWidth(300);}}, '1');
-//	  cp5.mapKeyFor(new ControlKey() {public void keyEvent() {accordion.setPosition(0,0);accordion.setItemHeight(190);}}, '2'); 
-//	  cp5.mapKeyFor(new ControlKey() {public void keyEvent() {accordion.setCollapseMode(ControlP5.ALL);}}, '3');
-//	  cp5.mapKeyFor(new ControlKey() {public void keyEvent() {accordion.setCollapseMode(ControlP5.SINGLE);}}, '4');
-//	  cp5.mapKeyFor(new ControlKey() {public void keyEvent() {cp5.remove("myGroup1");}}, '0');
-	  
 	  accordion.open(0,1,2);
 	  
 	  // use Accordion.MULTI to allow multiple group 
@@ -130,17 +107,6 @@ public class TheAccordion extends PApplet{
 	  // group can be open at a time.  
 	  // accordion.setCollapseMode(Accordion.SINGLE);
 	}
-	  
-
-	void radio(int theC) {
-	  switch(theC) {
-	    case(0):c=color(0,200);break;
-	    case(1):c=color(255,0,0,200);break;
-	    case(2):c=color(0, 200, 140,200);break;
-	    case(3):c=color(0, 128, 255,200);break;
-	    case(4):c=color(50,128);break;
-	  }
-	} 
 
 
 	void shuffle() {
