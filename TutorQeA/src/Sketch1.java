02167de45edd9d4229d19e6042ac1da0b15abb35
MATHEUS <MATHEUS@MATHEUS-PC>
2012-10-12 20:31:51 -0300

    Sketch1 incomplte.

--- TutorQeA/src/Sketch1.java
+++ TutorQeA/src/Sketch1.java
@@ -1,4 +1,11 @@
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import au.com.bytecode.opencsv.CSVReader;
 
 import controlP5.ControlEvent;
 import controlP5.ControlP5;
@@ -18,18 +25,34 @@ public class Sketch1 extends PApplet {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private String csvFilePath = "C:/Users/MATHEUS/workspace/rascunhoTutorQeA/TagsDictionary.csv";
+	private static HashMap<Integer, String> tagDictionary = new HashMap<Integer, String>();
+	private static HashMap<Integer, String> tagLinks = new HashMap<Integer, String>();
+
+	private ArrayList<Integer> selectedTags = new ArrayList<>();
+	private List<String> relatedTags = new ArrayList<>();
+
 	ControlP5 cp5;
 
 	ArrayList<DropdownList> lists = new ArrayList<>();
 
 	PFont font = createFont("arial", 12);
 
-	int actual_x = 20, actual_y = 80;
-
 	DropdownList d1;
 
 	public void setup() {
 		size(400, 400);
+
+		try {
+			readCSVFile(tagDictionary);
+			csvFilePath = "C:/Users/MATHEUS/workspace/rascunhoTutorQeA/TagLinks.csv";
+			readCSVFile(tagLinks);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(tagDictionary.get(1));
+		System.out.println(tagLinks.get(1));
 
 		noStroke();
 		cp5 = new ControlP5(this);
@@ -50,7 +73,9 @@ public class Sketch1 extends PApplet {
 	public void controlEvent(ControlEvent theEvent) {
 
 		if (theEvent.isGroup()) {
-			addDropDownList();
+			addDropDownList((int) theEvent.getValue());
+		} else {
+			System.out.println("fez algo");
 		}
 
 	}
@@ -61,23 +86,58 @@ public class Sketch1 extends PApplet {
 
 	public void input(String theText) {
 		clearList();
-		addDropDownList();
+		relatedTags = Arrays.asList(tagLinks.get(1).split(","));
+		System.out.println(tagDictionary.get(1));
+		addDropDownList(1);
 	}
 
-	private void addDropDownList() {
-		int x = 20+100*(lists.size()%3);
-		int y = 80+20*(floor(lists.size()/3));
-		DropdownList newD = cp5.addDropdownList("myList-d" + (lists.size()+1)).setPosition(x,y);
-		lists.add(newD);
-		try {
-			lists.get(lists.size() - 2).disableCollapse();
-		} catch (Exception e) {			
+	private void addDropDownList(int tagID) {
+
+		System.out.println("entrou com tagID: " + tagID);
+		int x = 20 + 100 * (lists.size() % 3);
+		int y = 80 + 20 * (floor(lists.size() / 3));
+
+		if (tagDictionary.containsKey(tagID)) {
+
+			DropdownList newD = cp5.addDropdownList(
+					"myList-d" + (lists.size() + 1)).setPosition(x, y);
+			for (String tag : relatedTags){
+				int id = Integer.valueOf(tag);
+				newD.addItem(tagDictionary.get(id), id);
+			}
+
+			lists.add(newD);
+			selectedTags.add(tagID);
+			try {
+				lists.get(lists.size() - 2).disableCollapse();
+			} catch (Exception e) {
+			}
+
 		}
 	}
+
 	public void clearList() {
 		for (DropdownList list : lists) {
 			list.remove();
 		}
 		lists = new ArrayList<>();
+		relatedTags = new ArrayList<>();
+	}
+
+	public void readCSVFile(HashMap<Integer, String> table) throws IOException {
+
+		CSVReader reader = new CSVReader(new FileReader(csvFilePath));
+
+		String[] nextLine;
+		while ((nextLine = reader.readNext()) != null) {
+			int key = Integer.valueOf(nextLine[0]);
+			if (table.containsKey(key)) {
+				table.put(Integer.valueOf(nextLine[0]),
+						table.get(Integer.valueOf(nextLine[0])) + ","
+								+ nextLine[1]);
+			} else {
+				table.put(Integer.valueOf(nextLine[0]), nextLine[1]);
+			}
+		}
 	}
 }
