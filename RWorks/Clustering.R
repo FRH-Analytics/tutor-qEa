75f231e00f11e653a965e1c02caedc3c6c6a4a78
Augusto QM <augustoqmacedo@gmail.com>
2012-11-08 00:59:09 -0300

    Organizing Clustering...

--- RWorks/Clustering.R
+++ RWorks/Clustering.R
@@ -20,104 +20,108 @@ run.Kmeans = function(features){
                scaledata = T)                    # scaledata: center the data
 }
 
-cluster.popularity = function(preprocessedDir, clusteringDir){
+clustering = function(preprocessedDir, clusteringDir, dimensionName, createFeaturesFunc){
     
-    featuresFile = paste(clusteringDir, "/difficulty.csv", sep = "")
+    print(noquote(paste("Clustering: ", dimensionName, "...", sep = "")))
     
-    if (file.exists(featuresFile)){
-        print(noquote("Loading Attributes..."))
-        features = read.csv(featuresFile)
-    }else{
-        print(noquote("Reading Data..."))
-        questions = read.csv(paste(preprocessedDir, "Questions.csv", sep = ""), header = T)
-        
-        print(noquote("Creating Attributes..."))
-        features = questions[,c("Id", "Score", "ViewCount", "CommentCount", "AnswerCount", "FavoriteCount")]
-        
-        print(noquote("Saving Attributes..."))
-        write.csv(features, file = paste(clusteringDir, "/popularity.csv", sep = ""), row.names = F)        
-    }
-    
-    # Exclude the ID from the cluster...
-    features = features[,-c(1)]
-    
-    # Run Clustering
-    print(noquote("Clustering..."))
-    model = run.Kmeans(features)
-    
-    # Plot Results
-    print(noquote("Plotting ScatterPlot..."))
-    png("output/Cluster-Kmeans-Popularity.png", width = 850, height = 900)
-    plot(data, col = (model$cluster + 2), 
-         main = paste("KMeans Clusters com", nrow(model$centers), "centro(s)"))
-    dev.off()
-}
-
-cluster.difficulty = function(preprocessedDir, clusteringDir){
-    
-    featuresFile = paste(clusteringDir, "/difficulty.csv", sep = "")
+    featuresFile = paste(clusteringDir, "/", tolower(dimensionName), ".csv", sep = "")
     
     if (file.exists(featuresFile)){
-        print(noquote("Loading Attributes..."))
+        print(noquote("Loading Features..."))
         features = read.csv(featuresFile)
     }else{
-        print(noquote("Reading Data..."))
-        questions = read.csv(paste(preprocessedDir, "Questions.csv", sep = ""), header = T)
-        answers = read.csv(paste(preprocessedDir, "Answers.csv", sep = ""), header = T)
+        features = createFeaturesFunc(preprocessedDir)
         
-        print(noquote("Creating Attributes..."))
-        features = data.frame(Id = questions$Id,
-                              AnswerCount = questions$AnswerCount,
-                              CommentCount = questions$CommentCount,
-                              HasAcceptedAnswer = (questions$AcceptedAnswerId != -1))
-        
-        features$TimeToMaxScoreAnswer = foreach(i = 1:nrow(features), .combine = rbind) %do%{
-            ans = answers[answers$ParentId == features$Id[i],]
-            
-            if (nrow(ans) > 0){
-                ans = ans[order(ans$Score, ans$CreationDate, decreasing=T),][1,]
-                val = as.numeric(difftime(ans$CreationDate , questions$CreationDate[i], units="hours"))
-                
-                # PROBLEM 1: Some answers have been created before the question!!! 
-                # (Cause: Question Merging...)
-                val = if (val < 0){0}else{val}
-            } else {
-                #print(i)
-                # PROBLEM 2: No answer! (Probably very difficult! We set -1...)
-                -1
-            }
-        }
-        
-        print(noquote("Saving Attributes..."))
-        write.csv(features, file = paste(clusteringDir, "/difficulty.csv", sep = ""), row.names = F)
+        print(noquote("Saving Features..."))
+        write.csv(features, file = featuresFile, row.names = F)
     }   
     
-    # Exclude the ID from the cluster...
-    features = features[,-c(1)]
+    # Excluding the ID and Cluster.KMeans collumns
+    if ("Cluster.KMeans" %in% colnames(features)){
+        features = features[,-ncol(features)]
+    }
     
     print(noquote("Clustering..."))
-    model = run.Kmeans(features[,-c(1)])
+    model = run.Kmeans(features[,-1])
+    
+    print(noquote("Persisting Clusters..."))
+    features$Cluster.KMeans = model$cluster
+    write.csv(features, file = featuresFile, row.names = F)
+    
+    print(noquote("Clustering: DONE!"))
+    print(noquote(""))
+}
+
+plot.cluster = function(clusteringDir, dimensionName){
+    
+    print(noquote(paste("Plotting: ", dimensionName, "...", sep = "")))
+    
+    features = read.csv(paste(clusteringDir, tolower(dimensionName), ".csv", sep = ""), header = T)
+    clusters = features$Cluster.KMeans
+    
+    # Exclude the Id and Cluster attributes
+    features = features[,-c(1, ncol(features))]
     
     # Plot Results
     print(noquote("Plotting ScatterPlot..."))
-    png("output/Cluster-Kmeans-Difficulty.png", width = 850, height = 900)
-    plot(features, col = (model$cluster + 2), 
-         main = paste("KMeans Clusters com", nrow(model$centers), "centro(s)"))
+    png(paste("output/Cluster-Kmeans-", dimensionName, ".png", sep = ""), width = 850, height = 900)
+    plot(features, col = (clusters + 2), 
+         main = paste(toupper(dimensionName), " clustering - KMeans with ", length(unique(clusters)), " center(s)", sep = ""))
     dev.off()
     
-    print(noquote("Plotting ScatterPlot 3D... (not so useful)"))
-    png("output/Cluster-Kmeans-Difficulty-3D.png", width = 850, height = 900)
-    scatterplot3d(x = features$AnswerCount, 
-                  y = features$CommentCount,
-                  z = features$HasAcceptedAnswer, 
-                  xlab = "x: Answer Count",
-                  ylab = "y: Comment Count",
-                  zlab = "z: Has Accepted Answer",
-                  color = (model$cluster + 2),
-                  main = paste("KMeans Clusters com", nrow(model$centers), "centro(s)"))
-    legend("bottomright",  pch = rep(1, 6), col=as.integer(sort(unique(model$cluster) + 2)), 
-           legend = paste("Cluster", sort(unique(model$cluster))))
-    dev.off()
+    print(noquote("Plotting: DONE!"))
+    print(noquote(""))
+}
+
+analyse.cluster = function(clusteringDir, dimensionName){
+    # TODO!
+    # Decision Tree
+}
+
+createFeatures.popularity = function(preprocessedDir){
+    print(noquote("Reading Data..."))
+    questions = read.csv(paste(preprocessedDir, "Questions.csv", sep = ""), header = T)
+    
+    print(noquote("Creating Attributes..."))
+    features = questions[,c("Id", "Score", "ViewCount", "CommentCount", 
+                            "AnswerCount", "FavoriteCount")]
+    
+    return(features)
+}
+    
+createFeatures.difficulty = function(preprocessedDir){
+    print(noquote("Reading Data..."))
+    questions = read.csv(paste(preprocessedDir, "Questions.csv", sep = ""), header = T)
+    answers = read.csv(paste(preprocessedDir, "Answers.csv", sep = ""), header = T)
+    
+    print(noquote("Creating Attributes..."))
+    features = data.frame(Id = questions$Id,
+                          AnswerCount = questions$AnswerCount,
+                          CommentCount = questions$CommentCount,
+                          HasAcceptedAnswer = (questions$AcceptedAnswerId != -1))
+    
+    features$TimeToMaxScoreAnswer = foreach(i = 1:nrow(features), .combine = rbind) %dopar%{
+        ans = answers[answers$ParentId == features$Id[i],]
+        
+        if (nrow(ans) > 0){
+            ans = ans[order(ans$Score, ans$CreationDate, decreasing=T),][1,]
+            val = as.numeric(difftime(ans$CreationDate , questions$CreationDate[i], units="hours"))
+            
+            # PROBLEM 1: Some answers have been created before the question!!! 
+            # (Cause: Question Merging...)
+            val = if (val < 0){0}else{val}
+        } else {
+            # PROBLEM 2: No answer! (Probably very difficult! We set -1...)
+            -1
+        }
+    }
+    
+    return(features)
+}
+
+createFeatures.longevity = function(preprocessedDir){
+    # TODO!
+    # List Attributes...
 }
 
 ####################################################
@@ -135,4 +139,8 @@ dir.create("../AllData/clustering", showWarnings=F)
 preprocessedDir = "../AllData/preprocessed/"
 clusteringDir = "../AllData/clustering/"
 
-# cluster.difficulty(preprocessedDir, clusteringDir)
+clustering(preprocessedDir, clusteringDir, "Difficulty", createFeatures.difficulty)
+clustering(preprocessedDir, clusteringDir, "Popularity", createFeatures.popularity)
+
+plot.cluster(clusteringDir, "Difficulty")
+plot.cluster(clusteringDir, "Popularity")
